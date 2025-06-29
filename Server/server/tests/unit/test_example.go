dd99667bdceeb9b1bcf09beb833b1a124fec2815
Someone <70491337+Someone1534@users.noreply.github.com>
2025-06-29 21:49:02 +0300

    Update test_example.go

---
+++ Server/server/tests/unit/test_example.go
@@ -0,0 +1,18 @@
+// example_test.go — положите рядом с кодом, который вы тестируете.
+package example
+
+import "testing"
+
+func TestPasses(t *testing.T) {
+	// Тест пройдёт, потому что 1 == 1.
+	if 1 != 1 {
+		t.Fatalf("ожидали 1 == 1")
+	}
+}
+
+func TestFails(t *testing.T) {
+	// Тест упадёт, потому что строки разные.
+	if "hello" != "goodbye" {
+		t.Fatalf(`ожидали "hello" == "goodbye" (тест демонстрационно провален)`)
+	}
+}
