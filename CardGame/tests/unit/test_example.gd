1c776fc0faf6ff9e44fdb98b06b226c7a00d30b2
Daniil Khasanov <da.khasanov@innopolis.university>
2025-06-30 11:38:00 +0300

    sfd

--- CardGame/tests/unit/test_example.gd
+++
@@ -1,9 +0,0 @@
-extends GutTest        # класс GutTest идёт с плагином GUT
-
-func test_passes():
-	# этот тест пройдёт, потому что 1 действительно равно 1
-	assert_eq(1, 1)
-
-func test_fails():
-	# этот тест упадёт, потому что строки не совпадают
-	assert_eq("hello", "goodbye")
